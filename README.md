cf7a24ee2c82237e8b697460f2a1a35323527113
Minji <wlsmscjswp@naver.com>
2021-07-27 10:28:20 +0900

    vmvr

--- README.md
+++ README.md
@@ -13,9 +13,11 @@ python run.py
 ## 현재 진행 상황 
 
 template 
+    common
+        - navbar.html  : 사이드 네비 바 관련 html 파일  
 - index.html ( 날짜 선택 page ) 
 - indextest.html ( 선택 기간 내 data 받아온 page -> chart 그려야 함 ) 
 
 run.py  ( flask 실행 파일 )  
-cal_day.py ( 기간 내 db 조회)
+cal_day.py ( 기간 내 db 조회)  
 datepick.py (  )
--- run.py
+++ run.py
@@ -34,25 +34,59 @@ def index():
 
     return render_template('index.html',data_list=data_list , today=str(to_day))
 
-@app.route('/detail_vm')
-def detail_vm():
-    to_day = datetime.datetime.today()      # index 9 까지 2021-07-26 
-    sql = "select * from flask where date like '"+ str(to_day)[:10] +"%'"
-
-    data_list = []
-    curs.execute(sql)
-    row = curs.fetchall()
-
-    for obj in row :
-        data_list.append( [obj[0],obj[1],obj[2]] )
-            # 날짜, CPU, disk 순서 
-
-    return render_template('detail_vm.html',data_list=data_list , today=str(to_day))
 
 @app.route('/indextest', methods=['GET'])
 def indextest():
     data_list, start, end = datepick(db_password)
     return render_template('indextest.html', data_list=data_list , start=start, end=end )
+
+
+# 오늘 날짜 계산하기! 
+to_day = datetime.datetime.today()  
+today_sql = "select * from flask where date like '"+ str(to_day)[:10] +"%'"
+today_data_list = []
+curs.execute(today_sql)
+row = curs.fetchall()
+for obj in row :
+    today_data_list.append( [obj[0],obj[1],obj[2]] )
+
+
+# vm page 
+@app.route('/vm_start')
+def vmstart():
+    global to_day, today_data_list
+    return render_template('vm_start.html',data_list=today_data_list , today=str(to_day))
+
+
+@app.route('/vm_lookup', methods=['GET'])
+def vmlookup():
+    data_list, start, end = datepick(db_password)
+    return render_template('vm_lookup.html', data_list=data_list , start=start, end=end )
+
+# vr page
+@app.route('/vr_start')
+def vrstart():
+    global to_day, today_data_list
+    return render_template('vr_start.html',data_list=today_data_list , today=str(to_day))
+
+
+@app.route('/vr_lookup', methods=['GET'])
+def vrlookup():
+    data_list, start, end = datepick(db_password)
+    return render_template('vr_lookup.html', data_list=data_list , start=start, end=end )
+
+# Cnode page 
+@app.route('/cnode_start')
+def cnodestart():
+    global to_day, today_data_list
+    return render_template('cnode_start.html',data_list=today_data_list , today=str(to_day))
+
+
+@app.route('/cnode_lookup', methods=['GET'])
+def cnodelookup():
+    data_list, start, end = datepick(db_password)
+    return render_template('cnode_lookup.html', data_list=data_list , start=start, end=end )
+
 
 if __name__ == '__main__' :
     app.run(host='127.0.0.1', port=5050, debug=True) 
